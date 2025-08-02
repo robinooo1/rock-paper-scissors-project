e3a4d6a837019699d37af703433ea9d127b21968
robinooo1 <robinsarian@hotmail.com>
2025-08-02 14:43:02 +0200

    Add pseudocode

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+console.log("Hello World"); /* Test */
+
+/* Pseudocode: Elaborate on each step! */
+// 1. Write the logic to get the computer choice.
+
+// 2. Write the logic to get the human choice.
+
+// 3. Declare the players score variables.
+
+// 4. Write the logic to play a single round.
+
+// 5. Write the logic to plat the entire game.
+
