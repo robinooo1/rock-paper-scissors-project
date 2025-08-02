52dd73427e0ce7353a5d80ebda1c3d6ea29d15b4
robinooo1 <robinsarian@hotmail.com>
2025-08-02 17:46:35 +0200

    Add function to human and computer choice: depending on outcomes, it returns if you win or lose. Add intialized score.

--- script.js
+++ script.js
@@ -36,8 +36,39 @@ function getHumanChoice() {
 console.log(getHumanChoice());
 
 // 3. Declare the players score variables.
+ let humanScore = 0;
+ let computerScore = 0;
 
 // 4. Write the logic to play a single round.
+// Function where it outputs scoreline from new variables: humanScore and computerScore
+// Variables will initialize with the value 0. 
+// If user input wins: add +1 point to user (humanScore +1 point)
+// If computer wins: add +1 point to computer (computerScore +1 point)
+// eg If getComputerChoice choose rock, and getHumanChoice choose paper, add 1+ point to computerScore...
+function playRound (humanChoice, computerChoice) {
+    if (humanChoice === "Rock" && computerChoice === "Paper") {
+        return "You lose! Paper beats Rock";
+    } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+        return "You win! Rock beats Scissors";
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        return "You win! Paper beats Rock";
+    } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+        return "You lose! Scissors beats Paper";
+    } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+        return "You lose! Rock beats Scissors";
+    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+        return "You win! Scissors beat Paper";
+    } else {
+        return "Tie! Both chose the same"
+    }
+}
+console.log(humanScore);
+console.log(computerScore);
+
+const humanSelection = getHumanChoice ();
+const computerSelection = getComputerChoice ();
+
+playRound(humanSelection, computerSelection);
 
 // 5. Write the logic to play the entire game.
 
